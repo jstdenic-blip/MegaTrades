d1b73a3d40797481d3cb270b1f1772a5f5134177
jstdenic-blip <jstdenic@gmail.com>
2025-11-22 16:21:41 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { ArrowRight, BarChart2, Globe, Shield, Smartphone } from 'lucide-react';
 import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
